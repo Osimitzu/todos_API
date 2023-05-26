d1488042e7d1e2dfea294fd6ce7fc3c16b4b82e9
Osimitzu <angel_aocc@hotmail.com>
2023-05-26 00:40:52 -0600

    Basic Server Completed

---
+++ src/app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+require("dotenv").config();
+const db = require("./utils/database");
+
+db.authenticate()
+  .then(() => console.log("Base de datos conectada (/OoO)/"))
+  .catch((err) => console.log(err));
+db.sync()
+  .then(() => console.log("Base de datos sincronizada (/OoO)/"))
+  .catch((err) => console.log(err));
+
+const PORT = process.env.PORT || 8000;
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Servidor OK (/OoO)/");
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor escuchando en el puerto ${PORT} (/OoO)/`);
+});
